b7d4b855536e048a4dd74c6d0ab3d6514d6ec47f
alex-pylypenko <52908028+alex-pylypenko@users.noreply.github.com>
2019-09-24 14:59:07 +0300

    json

--- Prog.kiev.ua/js/main.js
+++ Prog.kiev.ua/js/main.js
@@ -1,52 +1,69 @@
 
-
-let form = document.getElementById('submitForm');
 let users = [];
 
-function renderUsers(users) {
+let deleteElement = e => {
+    if ($(e.target).hasClass('remove-btn')) {
+        users.splice(0, 1);
+    }
+};
+
+let renderUsers = users => {
     let htmlStr = ``;
-    let deleteStr = ``;
     for (let index in users) {
         htmlStr += `<tr>
             <td>${+index + 1}</td>
             <td>${users[index].name}</td>
             <td>${users[index].email}</td>
             <td>${users[index].age}</td>
-            <td><img src="${users[index].picture}"></td>           
+            <td><img src="${users[index].picture}"></td>
+            <td><button class="remove-btn">Remove</button></td>
         </tr>`;
-        deleteStr = `<div style="margin-top: 75px">${users[index].delete}</div>`;
     }
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('age').value = '';
-    document.getElementById('picture').value = '';
-    document.querySelector('tbody').innerHTML = htmlStr;
-    document.querySelector('.right').innerHTML = deleteStr;
-}
-
-function addUser(e) {
-    e.preventDefault();
-    console.log('We are starting....', e);
-    let userObject = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        age: +document.getElementById('age').value,
-        picture: document.getElementById('picture').value,
-        delete: "X"
-    };
-
-
-    if (!userObject.name || !userObject.email || !userObject.age || !userObject.picture) {
-        alert('Fill all fields !!!');
-        return;
-    }
-
-    users.push(userObject);
-    renderUsers(users);
-}
-
-document.getElementById('delete').onclick = function () {
-    users[index].delete;
+    $('#name, #email, #age, #picture').val('');
+    $('tbody').html(htmlStr);
 };
 
-document.getElementById('send').addEventListener('click', addUser);
+let addUser = e => {
+    e.preventDefault();
+    console.log('We are starting....');
+    let userObject = {
+        name: $('#name').val(),
+        email: $('#email').val(),
+        age: +$('#age').val(),
+        picture: $('#picture').val()
+    };
+    if (!userObject.name || !userObject.email || !userObject.age || !userObject.picture) {
+        alert('Fill all fields');
+        return;
+    }
+    users.push(userObject);
+    renderUsers(users);
+};
+
+$('table tbody').on('click', deleteElement);
+
+$('#send').on('click', addUser);
+
+
+
+/*$.ajax({
+    url: 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json',
+    method: 'GET',
+    error: (e) => {
+        console.log(e);
+    },
+    success: (data) => {
+        for (var i = 0; i < data.length; i++) {
+            x = data.length;
+            $('.rates').html(data[i].txt + data[i].rate);
+        }
+    }
+});*/
+
+
+
+for (let i = 0; i < 61; i++) {
+    $.getJSON("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json", function (data) {
+        $('.rates').html(data[i].txt + data[i].rate);       
+    });
+}
